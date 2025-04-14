24e701a80a092131e0aa8fa8479bfd6a01036bde
João <joao@roboflow.com>
2025-04-14 19:29:30 -0300

    wip roboflow adapter

---
+++ src/adapters/roboflowAdapter.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { getEnv } from "@/utils/environment";
+
+const BASE_URL = "https://serverless.roboflow.com/";
+
+type BoundingBoxPrediction = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  confidence: number;
+};
+
+type InferImageResponse = {
+  inference_id: string;
+  time: number;
+  image: {
+    width: number;
+    height: number;
+  };
+  predictions: BoundingBoxPrediction[];
+};
+
+export const inferImage = async (
+  modelUrl: string,
+  imageB64: string
+): Promise<InferImageResponse> => {
+  const apiKey = getEnv("ROBOFLOW_API_KEY");
+
+  if (!apiKey) {
+    throw new Error("ROBOFLOW_API_KEY is not set");
+  }
+
+  console.log("Using API key:", apiKey.substring(0, 5) + "...");
+  console.log("Request URL:", `${BASE_URL}${modelUrl}`);
+
+  const response = await axios({
+    method: "POST",
+    url: `${BASE_URL}${modelUrl}`,
+    params: {
+      api_key: apiKey,
+    },
+    data: imageB64,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  return response.data;
+};
--- src/app/api/infer/route.ts
+++ src/app/api/infer/route.ts
@@ -1,13 +1,41 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { searchTopObjectDetectionTrainedDatasets } from "@/adapters/elasticAdapter";
 import { getAndCache } from "@/utils/cache";
+import { inferImage } from "@/adapters/roboflowAdapter";
 
-export async function POST() {
-  await getAndCache(
+export async function POST(request: NextRequest) {
+  const datasets = await getAndCache(
     "object-detection-datasets",
     searchTopObjectDetectionTrainedDatasets
   );
 
+  const firstDataset = datasets.hits.hits[0] as {
+    _source: {
+      url: string;
+      latestVersion: string;
+    };
+  };
+
+  console.log("\n\nFirst dataset\n\n");
+  console.log(JSON.stringify(firstDataset, null, 2));
+
+  const modelUrl = `${firstDataset._source.url}/${firstDataset._source.latestVersion}`;
+
+  console.log("\n\nModel URL\n\n");
+  console.log(modelUrl);
+
+  // image placeholder: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL0JzaLEO3u74JSvaqUjlGQIdfGD-1h1Rc-g&s
+
+  const imageB64 = await fetch(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL0JzaLEO3u74JSvaqUjlGQIdfGD-1h1Rc-g&s"
+  ).then((res) => res.arrayBuffer());
+
+  const imageB64String = Buffer.from(imageB64).toString("base64");
+  const response = await inferImage(modelUrl, imageB64String);
+
+  console.log("\n\nResponse\n\n");
+  console.log(JSON.stringify(response, null, 2));
+
   // Return a safe response without exposing sensitive data
-  return NextResponse.json({ success: true });
+  return NextResponse.json({ response });
 }
