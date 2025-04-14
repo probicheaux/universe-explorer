d0d58ce0375442db46ef2785631011884bf2199a
João <joao@roboflow.com>
2025-04-14 12:42:44 -0300

    dyamic prompt height

--- src/components/PromptArea.tsx
+++ src/components/PromptArea.tsx
@@ -32,7 +32,12 @@ export default function PromptArea({
 
   return (
     <div className="p-6 bg-gray-900/80 border-t border-gray-800">
-      <div ref={containerRef} className="relative min-h-[160px]">
+      <div
+        ref={containerRef}
+        className={`relative transition-all duration-300 ${
+          isEditing ? "min-h-[160px]" : "min-h-[80px]"
+        }`}
+      >
         {/* Static Prompt View */}
         <div
           className={`cursor-pointer transition-all duration-300 ${
