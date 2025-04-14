98c007118169d95173a65a70403ce624145b77e1
João <joao@roboflow.com>
2025-04-14 16:42:41 -0300

    puts universe explorer title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import UniverseExplorer from "../components/UniverseExplorer";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen p-8 bg-black relative">
-      <div className="relative h-full w-full rounded-xl bg-gray-900/50 backdrop-blur-md border border-gray-800 shadow-lg overflow-hidden">
+    <div className="flex flex-col gap-4 w-screen h-screen p-8 bg-black relative">
+      <h1 className="text-white text-2xl font-bold">Universe Explorer</h1>
+      <div className="relative flex-1 w-full rounded-xl bg-gray-900/50 backdrop-blur-md border border-gray-800 shadow-lg overflow-hidden">
         <UniverseExplorer />
       </div>
     </div>
