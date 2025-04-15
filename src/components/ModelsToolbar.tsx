1a05e134a83fbbc1251dbfaf40385184a157db8f
João <joao@roboflow.com>
2025-04-15 09:40:47 -0300

    better model cards, but I broke somethinfg

--- src/components/ModelsToolbar.tsx
+++ src/components/ModelsToolbar.tsx
@@ -69,18 +69,45 @@ const ModelCard = React.memo(
             </span>
             <div className="flex items-center gap-2 flex-shrink-0">
               {hasError ? (
-                <span className="text-xs px-1.5 py-0.5 rounded bg-red-900/50 text-red-400">
-                  Failed
-                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-red-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               ) : isComplete ? (
-                <span className="text-xs px-1.5 py-0.5 rounded bg-green-900/50 text-green-400">
-                  Complete
-                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-green-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               ) : (
-                <div className="animate-pulse">
-                  <span className="text-xs px-1.5 py-0.5 rounded bg-gray-800/50 text-gray-400">
-                    Processing...
-                  </span>
+                <div className="animate-spin">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </div>
               )}
             </div>
